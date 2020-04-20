f131f2c8614dd3f9dc00efb79311de0082a46564
Radoslav Hubenov <rrhubenov@gmail.com>
2020-04-20 14:41:52 +0300

    Fixes to the user manager

--- src/user/user_manager.cpp
+++ src/user/user_manager.cpp
@@ -5,10 +5,12 @@ UserManager::UserManager() {
 }
 
 void UserManager::addUser(User* user) {
-    this->users[this->users_count+1];
+    this->users[this->users_count+1] = user;
     this->users_count++;
 }
 
 UserManager::~UserManager() {
-    delete[] users;
+    for(unsigned i = 0; i < this->users_count; i++) {
+        delete this->users[i];
+    }
 }
